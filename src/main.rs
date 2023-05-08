a23177f654b34bdeebbcee58db33d96621e4fa09
Ji Chen <jaykchen@gmail.com>
2023-05-07 20:40:06 -0400

    chain vs single step

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,71 @@
-use llm_chain::chains::map_reduce::Chain;
+use dotenv::dotenv;
+use std::env;
+
+use llm_chain::chains::{map_reduce, sequential};
 use llm_chain::step::Step;
-use llm_chain::{executor, parameters, prompt, Parameters};
-use llm_chain::text_splitter::{NaiveWhitespaceSplitter, TextSplitter};// use llm_chain_openai::executor;
+use llm_chain::traits::Executor;
+use llm_chain::{parameters, prompt, Parameters};
+use llm_chain_openai::chatgpt::PerExecutor;
+
+use tiktoken_rs::cl100k_base;
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create a new ChatGPT executor with the default settings
-    let exec = executor!()?;
+    dotenv().ok();
+    let openai_key = env::var("OPENAI_API_KEY").unwrap();
 
-    // Create the "map" step to summarize an article into bullet points
+    let exec = llm_chain_openai::chatgpt::Executor::new_with_options(
+        Some(PerExecutor {
+            api_key: Some(openai_key),
+        }),
+        None,
+    )?;
+
     let map_prompt = Step::for_prompt_template(prompt!(
-        "You are a bot for summarizing wikipedia articles, you are terse and focus on accuracy",
-        "Summarize this article into bullet points:\n{{text}}"
+        "You are a senior software developer. You will review a source code file and its patch",
+        "The following is a patch. Please summarize key changes.\n{{text}}"
     ));
 
     // Create the "reduce" step to combine multiple summaries into one
     let reduce_prompt = Step::for_prompt_template(prompt!(
-        "You are a diligent bot that summarizes text",
-        "Please combine the articles below into one summary as bullet points:\n{{text}}"
+        "You are a senior software developer. You have reviewed the source code files and its patches, created some interim notes and now create a summary",
+        "Please combine the pieces and make a summary:\n{{text}}"
     ));
 
-    // Create a map-reduce chain with the map and reduce steps
-    let chain = Chain::new(map_prompt, reduce_prompt);
+    let bpe = cl100k_base()?;
+    //  Create a map-reduce chain with the map and reduce steps
+    let map_chain = map_reduce::Chain::new(map_prompt, reduce_prompt);
 
-    // Load the content of the article to be summarized
     let article = include_str!("article_to_summarize.md");
+    // let article = include_str!("article_to_summarize.md");
+    // let tokens = bpe.encode_ordinary(&article);
+    let tokens = bpe.encode_with_special_tokens(article);
+    let chunked = tokens
+        .chunks(1800)
+        .map(|c| bpe.decode(c.to_vec()).unwrap())
+        .collect::<Vec<String>>();
 
-    // Create a vector with the Parameters object containing the text of the article
-    let splitter =  NaiveWhitespaceSplitter;
-    
-  let artcle_parts = splitter.split_text(article, 200, 20);
+    let docs = chunked.iter().map(|c| parameters!(c)).collect::<Vec<_>>();
 
-if let Ok(chunks) = artcle_parts {
-    let docs = chunks
-        .into_iter()
-        .map(|part| parameters!(part))
-        .collect::<Vec<_>>();
-    // Run the chain with the provided documents and an empty Parameters object for the "reduce" step
-    let res = chain.run(docs, Parameters::new(), &exec).await.unwrap();
+    // let docs = vec![parameters!(article)];
 
-    // Print the result to the console
+    // for d in docs.clone().iter() {
+    //     let text = d.get_text().unwrap().to_string();
+    //     let head = text.chars().take(50).collect::<String>();
+    //     let tokens = bpe.encode_with_special_tokens(&text);
+
+    //     println!("{:?}, {}", head, tokens.len());
+    // }
+    let res = map_chain.run(docs, Parameters::new(), &exec).await.unwrap();
     println!("{}", res);
-}
+
+    let step = Step::for_prompt_template(prompt!(
+        "You're project manager",
+        "Report to your superior what you've just done in one sentence:\n{{text}}"
+    ));
+
+    let res = step.run(&parameters!(res.to_string()), &exec).await?;
+    println!("{}", res);
+
     Ok(())
 }
